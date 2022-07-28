1294a5fcbe7f5c15e5e3836b3a2089ebda1098f9
robin <robinnagpal.tiet@gmail.com>
2022-07-28 10:43:18 -0400

    Add questions in readings file

--- src/validation/validateReadingsFiles.ts
+++ src/validation/validateReadingsFiles.ts
@@ -4,6 +4,7 @@ import { throwValidationError } from './throwValidationError';
 import { readFilesSync } from '../utils/readFilesSync';
 import YAML from 'yaml';
 import readingsFileSchema from './schemas/readingsFileSchema.json';
+import readingsQuestionSchema from './schemas/readingsQuestionSchema.json';
 
 export function validateReadingsFiles(readingsDirectory: string) {
   const readingFiles = readFilesSync(readingsDirectory);
@@ -11,6 +12,7 @@ export function validateReadingsFiles(readingsDirectory: string) {
     const file = fs.readFileSync(questionFile.filepath, 'utf8');
     const readingJson = YAML.parse(file);
     const v = new Validator();
+    v.addSchema(readingsQuestionSchema, '/ReadingsQuestionSchema');
     const res = v.validate(readingJson, readingsFileSchema);
     if (!res.valid) {
       throwValidationError(questionFile.filepath, res.errors);
